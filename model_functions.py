fb4e64ed3a95eae1fb1633652f0d6ad8c77a48b7
Vlatko <vlatko.vilovic@gmail.com>
2024-06-25 19:59:02 +0200

    updated model_functions.py file

--- model_functions.py
+++ model_functions.py
@@ -19,33 +19,3 @@ def adf_test_p_values (df):
     return (p_vals)
 
 
-# Create train and test datasets for the different stores
-
-def store_dataset (train, test):
-
-    # Training set 
-
-    groups = train[-train["store_name"].isin(["Neuer Kamp","Altona","Jungfernstieg","Hamburg Hauptbahnhof","KaDeWe"])].groupby("store_name")[["days_back","month","total_amount","lag1"]]
-
-    group_dict = {i:j for i, j in groups}
-
-    for store in group_dict:
-        globals()[store.lower()] = group_dict[store]
-
-    for store in group_dict:
-        globals()[f'{store.lower()}_y'] = group_dict[store]['total_amount']
-
-
-    # Test set
-
-    groups_test = test[-test["store_name"].isin(["Neuer Kamp","Altona","Jungfernstieg","Hamburg Hauptbahnhof","KaDeWe"])].groupby("store_name")[["days_back","month","total_amount","lag1"]]
-
-    group_dict_test = {i:j for i, j in groups_test}
-
-    for store in group_dict_test:
-        globals()[f"{store.lower()}_test"] = group_dict_test[store]
-
-    for store in group_dict_test:
-        globals()[f'{store.lower()}_y_test'] = group_dict_test[store]['total_amount']
-    
-    return danziger, danziger_test
